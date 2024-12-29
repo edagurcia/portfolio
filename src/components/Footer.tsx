0c37be2238e3b3aa4fc215f8dd78c45d8b32dd9d
Edmundo Domínguez Agurcia <edagurcia@gmail.com>
2024-12-29 16:32:28 -0600

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,8 +9,11 @@ export const Footer = () => {
   return (
     <footer className="h-20 bg-neutral-900 flex items-center justify-center">
       <div className="flex flex-col items-center">
-        <p className="text-center text-base text-neutral-300">
-          &copy; {currentYear}{" "}
+        <p className="text-center text-sm text-neutral-300">
+          &copy; {currentYear},{" "}
+          <span className="text-xs uppercase tracking-tight">
+            Edmundo Domínguez,
+          </span>{" "}
           {isEnglish
             ? "All rights reserved."
             : "Todos los derechos reservados."}
