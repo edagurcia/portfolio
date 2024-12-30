e885074395afb090482aa7ad0c9a2e21a8b636b6
Edmundo Domínguez Agurcia <edagurcia@gmail.com>
2024-12-29 18:54:52 -0600

    created middleware

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  // Agrega los headers CORS
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  return response;
+}
+
+export const config = {
+  matcher: "/:path*",
+};
